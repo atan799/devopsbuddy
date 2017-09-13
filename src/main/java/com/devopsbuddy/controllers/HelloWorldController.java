4f58f604116f88a0a9ea83fbf86c3d6ec1df389c
Alex Tan <atan799@gmail.com>
2017-09-13 10:44:46 -0500

    Added HellowWorldCOntroller.java

--- src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.xml.ws.RequestWrapper;
 
+/**
+ * Created by alex
+ */
 @Controller
 public class HelloWorldController {
 
     @RequestMapping("/")
     public String sayHello() {
+
         return ("index");
     }
 }
