93bf651f004f401b4107dffa794d0d199d1b6b17
Alex Tan <atan799@gmail.com>
2017-09-14 23:16:56 -0500

    refactored boilerplate code to Typeleaf fragments

--- src/main/java/com/devopsbuddy/web/controllers/indexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/indexController.java
@@ -3,11 +3,11 @@ package com.devopsbuddy.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class indexController {
     @RequestMapping
     public String home() {
-
         return "index";
     }
 }
